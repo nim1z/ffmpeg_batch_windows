72517b3e95e88ac7b6201ae5d94a9407cbddfef7
nim1z <nim1z@outlook.jp>
2020-10-24 19:16:03 +0900

    Update and rename ffmpeg_avc_SW_faster.bat to ffmpeg_avc_SW_fast.bat

---
+++ ffmpeg_avc_SW_fast.bat
@@ -0,0 +1,2 @@
+ffmpeg -i "%~1" -c:v libx264 -crf 18 -preset fast -vf format=yuv420p -c:a aac -b:a 256k "%~n1_AVC.mp4"
+pause
--- ffmpeg_avc_SW_faster.bat
+++
@@ -1,2 +0,0 @@
-ffmpeg -i "%~1" -c:v libx264 -crf 18 -preset faster -vf format=yuv420p -c:a aac -b:a 256k "%~n1_AVC.mp4"
-pause
